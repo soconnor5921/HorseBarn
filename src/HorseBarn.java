2426ac817a8f353d78fff7dc10858c35989fcf4b
BT_1N3_32 <soconnor5921@bths.edu>
2018-10-25 10:16:45 -0400

    10/25

--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -1,9 +1,3 @@
-public interface Horse
-{
-    String getName();
-    int getWeight();
-}
-
 public class HorseBarn
 {
     private Horse[] spaces;
@@ -24,6 +18,20 @@ public class HorseBarn
 
     public void consolidate()
     {
-
+        for(int i = 0; i < this.spaces.length-1; i++)
+        {
+            if(this.spaces[i] == null)
+            {
+                for(int j = i+1; j < this.spaces.length; j++)
+                {
+                    if(this.spaces[j] != null)
+                    {
+                        this.spaces[i] = this.spaces[j];
+                        this.spaces[j] = null;
+                        j = this.spaces.length;
+                    }
+                }
+            }
+        }
     }
 }
--- src/HorseRunner.java
+++ src/HorseRunner.java
@@ -16,9 +16,9 @@ public class HorseRunner
         System.out.println(barn);
         System.out.println(" Dusty Trail is in space: " + barn.findHorseSpace("Dusty Trail"));
 
-        /**barn.consolidate();
+        barn.consolidate();
 
         System.out.println(barn);
-        Syatem.out.println(" Dusty  Trail is now in space: " + barn.findHorseSpace("Dusty Trail"));
+        System.out.println(" Dusty  Trail is now in space: " + barn.findHorseSpace("Dusty Trail"));
     }
 }
